ed808da3a54ce71301a8da37f625f3bc4535809c
Saman Mehr Ali Pur <saman_mehr_ali_poor@tutanota.com>
2022-10-07 16:08:01 +0330

    add queries into UserOrganizationDao class

--- src/main/java/dao/UserOrganizationDao.java
+++ src/main/java/dao/UserOrganizationDao.java
@@ -6,6 +6,15 @@ import database.Database;
 import model.UserOrganization;
 
 public class UserOrganizationDao {
+   private final String INSERT_QUERY = "insert into user_organization (createdDate,registerDate,user_id," +
+           "organization_id) values(?,?,?,?)";
+   private final String DELETE_QUERY = "delete from user_organization where id=?";
+   private final String UPDATE_QUERY = "update user_organization set" +
+           "registerDate=?,user_id=?,organization_id=?";
+
+
+
+
     public void create(UserOrganization userOrganization) {
         List<UserOrganization> organizationDb = Database.getUserOrganizationDB();
         organizationDb.add(userOrganization);
